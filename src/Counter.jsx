4e2f55f80bf8896cd1b3e3c4f247c34a3a50c6dc
Pakize Bozkurt <pakiozihak@gmail.com>
2022-11-25 19:25:00 +0000

    Worked out the Counter App

---
+++ src/Counter.jsx
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import {Button, Card} from "react-bootstrap";
+
+function Counter() {
+const[count1,setCount1] = useState(0);
+ const[count2,setCount2] = useState(0);
+ const handleCount1=()=>{
+    setCount1(count1+1)
+ }
+ const handleCount2=()=>{
+    setCount2(count2+1)
+ }
+  return (
+    <div>
+   <h1>Total: {count1+count2}</h1>
+        <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                <Card.Title>Count1: {count1}</Card.Title>
+                <Button onClick={handleCount1}>Add</Button>
+            </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                <Card.Title>Count2: {count2}</Card.Title>
+                <Button onClick={handleCount2}>Add</Button>
+            </Card.Body>
+        </Card>
+    </div>
+  )
+}
+
+export default Counter;
