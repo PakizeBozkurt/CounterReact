025c9bdd5118c37ac3745794d0c78bcbcd3f5510
Pakize Bozkurt <pakiozihak@gmail.com>
2022-11-29 09:21:45 +0000

    Styling

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -14,21 +14,23 @@ const[count1,setCount1] = useState(0);
  const customStyle = {
      color: "black",
      fontSize: "20px",
-     border: "1px solid black"
+     border: "1px solid yellow",
+  
  }
+  
   return (
     <div>
    <h1 style= {customStyle} className='h1'>Total: {count1+count2}</h1>
         <Card style={{ width: '18rem' }}>
             <Card.Body>
                 <Card.Title>Count1: {count1}</Card.Title>
-                <Button onClick={handleCount1}>Add</Button>
+                <Button style={customStyle} className='btn' onClick={handleCount1}>Add +</Button>
             </Card.Body>
         </Card>
         <Card style={{ width: '18rem' }}>
             <Card.Body>
                 <Card.Title>Count2: {count2}</Card.Title>
-                <Button onClick={handleCount2}>Add</Button>
+                <Button className='btn' onClick={handleCount2}>Add +</Button>
             </Card.Body>
         </Card>
     </div>
